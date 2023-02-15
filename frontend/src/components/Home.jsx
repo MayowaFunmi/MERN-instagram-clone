9a63c57a276db35e60334f048781026f9c5cc56c
Akinade Mayowa <akinade.mayowa@gmail.com>
2023-02-15 21:56:13 +0100

    added home page

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,7 +1,40 @@
 import React from 'react';
+import user1 from '../images/user1.jpg';
+import user2 from '../images/user2.jpg';
+import './Home.css';
 
-function Home() {
-  return <div>Home</div>;
-}
+const Home = () => {
+  return (
+    <div className="home">
+      {/* card */}
+      <div className="card">
+        {/* card header */}
+        <div className="card-header">
+          <div className="card-pic">
+            <img src={user1} alt="" />
+          </div>
+          <h5>User 1</h5>
+        </div>
+        {/* card image */}
+        <div className="card-image">
+          <img src={user2} alt="" />
+        </div>
+        {/* card content */}
+        <div className="card-content">
+          <span class="material-symbols-outlined">favorite</span>
+          <p>1 Like</p>
+          <p>This is amazing</p>
+        </div>
+
+        {/* comment */}
+        <div className="add-comment">
+          <span class="material-symbols-outlined">mood</span>
+          <input type="text" placeholder="Add a comment" />
+          <button className="comment">Post</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
--- frontend/src/components/SignIn.jsx
+++ frontend/src/components/SignIn.jsx
@@ -1,11 +1,53 @@
 import React, { useState } from 'react';
 import './SignIn.css';
 import logo from '../images/insta-logo.jpeg';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 const SignIn = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const notifyError = (msg) => toast.error(msg);
+  const notifySuccess = (msg) => toast.success(msg);
+  const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  const passwordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+
+  const postData = () => {
+    // validating email
+    if (!emailRegex.test(email)) {
+      notifyError('Email is invalid!!!');
+      return;
+    } else if (!passwordRegex.test(password)) {
+      notifyError(
+        'Password must contain at least 8 characters including one number, one uppercase letter, one lower case letter and a special character!'
+      );
+      return;
+    }
+    // sending data to server
+    fetch('http://localhost:5000/signin', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.error) {
+          notifyError(data.error);
+        } else {
+          notifySuccess(data.message);
+          navigate('/');
+        }
+      });
+  };
+
   return (
     <div className="signIn">
       <div>
@@ -35,7 +77,14 @@ const SignIn = () => {
               }}
             />
           </div>
-          <input type="submit" id="login-btn" value="LogIn" />
+          <input
+            type="submit"
+            id="login-btn"
+            value="LogIn"
+            onClick={() => {
+              postData();
+            }}
+          />
         </div>
         <div className="loginForm2">
           Don't have an account?
